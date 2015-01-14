abd039354a4158e6d8d752b228e97e609484b7a2
xiewulong <xiewulong@vip.qq.com>
2015-01-14 09:20:47 +0800

    crete table

--- migrations/m150108_115246_payment.php
+++ migrations/m150108_115246_payment.php
@@ -24,19 +24,9 @@ class m150108_115246_payment extends Migration{
 			'created_at' => Schema::TYPE_INTEGER . ' not null comment "创建时间"',
 			'updated_at' => Schema::TYPE_INTEGER . ' not null comment "更新时间"',
 		], $tableOptions . ' comment="支付记录"');
-
-		$this->createTable('{{%payment_notify}}', [
-			'mode' => Schema::TYPE_STRING . '(50) not null comment "第三方支付类型"',
-			'tid' => Schema::TYPE_BIGINT . ' not null comment "第三方支付端流水号"',
-			'pid' => Schema::TYPE_BIGINT . ' not null comment "支付记录id"',
-			'status' => Schema::TYPE_BOOLEAN . ' not null default 0 comment "支付结果状态"',
-			'data' => Schema::TYPE_TEXT . ' not null comment "消息通知数据(json)"',
-			'created_at' => Schema::TYPE_INTEGER . ' not null comment "接收时间"',
-		], $tableOptions . ' comment="第三方支付消息通知记录"');
 	}
 
 	public function down(){
-		$this->dropTable('{{%payment_notify}}');
 		$this->dropTable('{{%payment}}');
 	}
 
