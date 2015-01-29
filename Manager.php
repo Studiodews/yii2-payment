9fb407b14b4da821a3fd743d00e9cdec73aa3442
xiewulong <xiewulong@vip.qq.com>
2015-01-29 13:42:41 +0800

    payment

--- Manager.php
+++ Manager.php
@@ -5,7 +5,7 @@
  * https://github.com/xiewulong/yii2-payment
  * https://raw.githubusercontent.com/xiewulong/yii2-payment/master/LICENSE
  * create: 2015/1/10
- * update: 2015/1/13
+ * update: 2015/1/29
  * version: 0.0.1
  */
 
